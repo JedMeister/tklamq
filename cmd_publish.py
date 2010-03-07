ac3e17b1df77996abc938fd625105fb562c73587
Alon Swartz <alon@sterilesecurity.com>
2010-03-07 09:39:29 +0200

    added input option to cmd_publish: -i --input=PATH   content to send (- for stdin)

--- cmd_publish.py
+++ cmd_publish.py
@@ -2,12 +2,13 @@
 """
 Arguments:
 
-    exchange        name of exchange
-    routing_key     interpretation of routing key depends on exchange type
+    exchange            name of exchange
+    routing_key         interpretation of routing key depends on exchange type
 
 Options:
 
-    -e --encrypt    encrypt message using secret TKLAMQ_SECRET
+    -i --input=PATH     content to send (- for stdin)
+    -e --encrypt        encrypt message using secret TKLAMQ_SECRET
 """
 
 import os
@@ -32,14 +33,18 @@ def fatal(s):
 
 def main():
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'he')
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'i:eh', ['input='])
     except getopt.GetoptError, e:
         usage(e)
 
+    opt_input = None
     opt_encrypt = False
     for opt, val in opts:
         if opt == '-h':
             usage()
+
+        if opt in ('-i', '--input'):
+            opt_input = val
 
         if opt in ('-e', '--encrypt'):
             opt_encrypt = True
@@ -51,12 +56,17 @@ def main():
     if opt_encrypt and not secret:
         fatal('TKLAMQ_SECRET not specified, cannot encrypt')
 
+    # unset secret if encryption was not specified
     if not opt_encrypt:
         secret = None
 
-    exchange, routing_key = args
+    content = ''
+    if opt_input == '-':
+        content = sys.stdin.read()
+    elif opt_input:
+        content = file(opt_input).read()
 
-    content = sys.stdin.read()
+    exchange, routing_key = args
     message = encode_message(content, secret=secret)
 
     conn = connect()
