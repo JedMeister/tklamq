47ff32650c2cd3c5a89ac1d0cc5a3f5e17b2a09c
Alon Swartz <alon@sterilesecurity.com>
2010-03-12 14:27:45 +0200

    removed script execution
    rational:
        - now handled by hubclient

--- cmd_consume.py
+++ cmd_consume.py
@@ -10,11 +10,6 @@ If message content is encrypted, TKLAMQ_SECRET will be used as decryption key
 import os
 import sys
 
-import tempfile
-import shutil
-
-import executil
-
 from tklamq.amqp import __doc__ as env_doc
 from tklamq.amqp import connect, decode_message
 
@@ -27,27 +22,7 @@ def fatal(s):
     print >> sys.stderr, "error: " + str(s)
     sys.exit(1)
 
-class TempFile(file):
-    def __init__(self, prefix='tmp', suffix=''):
-        fd, path = tempfile.mkstemp(suffix, prefix)
-        os.close(fd)
-        self.path = path
-        self.pid = os.getpid()
-        file.__init__(self, path, "w")
-
-    def __del__(self):
-        if self.pid == os.getpid():
-            os.remove(self.path)
-
-def execute(s):
-    fh = TempFile(prefix="tklamq-")
-    fh.writelines(s)
-    fh.close()
-
-    os.chmod(fh.path, 0750)
-    executil.system(fh.path)
-
-def decrypt_execute_callback(message_data, message):
+def decrypt_callback(message_data, message):
     encrypted = message_data['encrypted']
     secret = os.getenv('TKLAMQ_SECRET', None)
 
@@ -55,13 +30,7 @@ def decrypt_execute_callback(message_data, message):
         fatal('TKLAMQ_SECRET not specified, cannot decrypt cipher text')
 
     sender, content, timestamp = decode_message(message_data, secret)
-
-    # only execute if encrypted (trusted sender) and includes shebang
-    if encrypted and content.startswith("#!"):
-        execute(content)
-        print "message processed (%s)" % timestamp.isoformat(" ")
-    else:
-        print content
+    print content
 
     message.ack()
 
@@ -72,7 +41,7 @@ def main():
     queue = sys.argv[1]
     
     conn = connect()
-    conn.consume(queue, callback=decrypt_execute_callback)
+    conn.consume(queue, callback=decrypt_callback)
 
 if __name__ == "__main__":
     main()
